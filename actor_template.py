7600f8e8e25c9eb71ea050c14f6713e39b2c59ab
Jean-Luc Stevens <jeanlucstevens@ymail.com>
2024-10-20 11:11:27 +1100

    add base files

---
+++ actor_template.py
@@ -0,0 +1,52 @@
+import unittest
+
+
+
+TEST_ACTOR_TEMPLATE = {
+    "str": {
+    "value": 20,
+    "proficient": 1,
+    },
+    "dex": {
+    "value": 12,
+    "proficient": 0,
+    },
+    "con": {
+    "value": 14,
+    "proficient": 1,
+    },
+    "int": {
+    "value": 10,
+    "proficient": 0,
+    },
+    "wis": {
+    "value": 12,
+    "proficient": 0,
+    },
+    "cha": {
+    "value": 14,
+    "proficient": 0,
+    }
+}
+
+
+
+
+
+
+
+
+
+
+class TestBaseStats(unittest.TestCase):
+
+
+
+
+
+
+
+
+
+
+
